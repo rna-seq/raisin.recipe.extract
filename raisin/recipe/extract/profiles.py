1ae99a283a0e3eb7ead0faaee0d11c8b2d054ff0
Maik R��der <maikroeder@gmail.com>
2012-02-01 21:53:55 +0000

    =add more parameters

--- raisin/recipe/extract/profiles.py
+++ raisin/recipe/extract/profiles.py
@@ -6,28 +6,46 @@ def parse_profile_file(file):
     parser = ConfigParser.RawConfigParser()
     parser.optionxform = lambda s: s
     parser.readfp(file)
-    profile = {}
-    if "pipeline" in parser.sections():
-        profile = dict(parser.items("pipeline"))
-    else:
-        print file
-        raise AttributeError("The profile is missing a pipeline secion")
-    return profile
-
+    profiles = {}
+    for section in parser.sections():
+        profiles[section] = dict(parser.items(section))
+    return profiles
+    
+def extract_profiles(parsed):
+    pipelines = set([])
+    for key, value in parsed.items():
+        if value.has_key('pipeline'):
+            pipelines.add(value['pipeline'])
+        elif value.has_key('accession'):
+            pipelines.add('pipeline')
+    for pipeline in pipelines:
+        if pipeline == 'pipeline':
+            profile = parsed['pipeline'].copy()
+            profile['pipeline_id'] = 'pipeline'
+        else:
+            profile = parsed['pipeline'].copy()
+            profile.update(parsed[pipeline])
+            profile['pipeline_id'] = pipeline
+        yield profile
 
 def main(workspace):
     profile_files = [f for f in glob.glob('../../profiles/*/*.cfg')]
 
     headers = ["project_id",
-               "mapper",
-               "mismatches",
-               "projectid",
-               "db",
-               "cluster",
-               "host",
-               "threads",
-               "template",
-               "commondb",
+               "pipeline_id",
+               "MAPPER",
+               "MISMATCHES",
+               "PROJECTID",
+               "DB",
+               "CLUSTER",
+               "HOST",
+               "THREADS",
+               "TEMPLATE",
+               "COMMONDB",
+               "ANNOTATION",
+               "GENOMESEQ",
+               "PREPROCESS",
+               "PREPROCESS_TRIM_LENGTH"
                ]
 
     template = '\t'.join(['%s'] * len(headers)) + '\n'
@@ -35,22 +53,27 @@ def main(workspace):
     output_file.write('\t'.join(headers) + '\n')
 
     parsed_profiles = {}
-    for profile in profile_files:
-        profile_file = open(profile, 'r')
-        parsed_profiles[profile] = parse_profile_file(profile_file)
-
-    for key, profile in parsed_profiles.items():
-        project_id = os.path.split(os.path.split(key)[0])[-1]
-        output_file.write(template % (project_id,
-                                      profile['MAPPER'],
-                                      profile.get('MISMATCHES', ''),
-                                      profile['PROJECTID'],
-                                      profile['DB'],
-                                      profile.get('CLUSTER', ''),
-                                      profile['HOST'],
-                                      profile['THREADS'],
-                                      profile['TEMPLATE'],
-                                      profile['COMMONDB']
-                                      )
-                            )
+    for profile_file in profile_files:
+        print profile_file
+        file = open(profile_file, 'r')
+        parsed = parse_profile_file(file)
+        project_id = os.path.split(os.path.split(profile_file)[0])[-1]
+        for profile in extract_profiles(parsed):
+            output_file.write(template % (project_id,
+                                          profile['pipeline_id'],
+                                          profile['MAPPER'],
+                                          profile['MISMATCHES'],
+                                          profile['PROJECTID'],
+                                          profile['DB'],
+                                          profile.get('CLUSTER', ''),
+                                          profile['HOST'],
+                                          profile['THREADS'],
+                                          profile['TEMPLATE'],
+                                          profile['COMMONDB'],
+                                          profile['ANNOTATION'],
+                                          profile['GENOMESEQ'],
+                                          profile.get('PREPROCESS', ''),
+                                          profile.get('PREPROCESS_TRIM_LENGTH', '')
+                                          )
+                                )
 
