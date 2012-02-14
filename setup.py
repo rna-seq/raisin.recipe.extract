11660469f08970567c58652e86a5a3f69a5ca312
Maik R��der <maikroeder@gmail.com>
2012-02-14 14:49:43 +0000

    =add dependency on RestrictedPython

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name='raisin.recipe.extract',
       install_requires=['setuptools',
                         'zc.buildout',
                         # -*- Extra requirements: -*-
+                        RestrictedPython,
                         ],
       entry_points=entry_points,
       )
