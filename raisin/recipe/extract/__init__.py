85b5fb0f5904ce95ca0373783f585562c4d61449
Maik R��der <maikroeder@gmail.com>
2012-02-14 15:21:06 +0000

    =move Recipe here

--- raisin/recipe/extract/__init__.py
+++ raisin/recipe/extract/__init__.py
@@ -1,3 +1,28 @@
 # -*- coding: utf-8 -*-
-"""Recipe apache"""
-from recipe import Recipe
+"""Recipe raisin.recipe.extract"""
+
+import accessions
+import annotations
+import files
+import genomes
+import profiles
+
+
+class Recipe(object):
+
+    def __init__(self, buildout, name, options):
+        self.buildout = buildout
+        self.name = name
+        self.options = options
+
+    def install(self):
+        buildout_directory = self.buildout['buildout']['directory']
+        workspace = self.options['workspace']
+        accessions.main(buildout_directory, workspace)
+        annotations.main(buildout_directory, workspace)
+        files.main(buildout_directory, workspace)
+        genomes.main(buildout_directory, workspace)
+        profiles.main(buildout_directory, workspace)
+
+    def update(self):
+        return self.install()
