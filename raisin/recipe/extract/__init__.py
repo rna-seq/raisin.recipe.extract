fe1e629c75247c9d27c997558f07eb6c4e18293a
Maik R��der <maikroeder@gmail.com>
2012-02-21 15:26:32 +0000

    =pass workspace

--- raisin/recipe/extract/__init__.py
+++ raisin/recipe/extract/__init__.py
@@ -8,6 +8,7 @@ from raisin.recipe.extract import annotations
 from raisin.recipe.extract import files
 from raisin.recipe.extract import genomes
 from raisin.recipe.extract import profiles
+from raisin.recipe.extract import replicates
 
 
 class Recipe(object):
@@ -27,12 +28,13 @@ class Recipe(object):
         if not os.path.exists(annotations_file):
             raise AttributeError("annotations_file not found: %s" % annotations_file)
         annotations.main(buildout_directory, workspace, annotations_file)
-        files.main(buildout_directory, workspace)
+        files.main(self.buildout, buildout_directory, workspace)
         genomes_file = self.options['genomes_file']
         if not os.path.exists(genomes_file):
             raise AttributeError("genomes_file not found: %s" % genomes_file)
         genomes.main(buildout_directory, workspace, genomes_file)
         profiles.main(self.buildout, buildout_directory, workspace)
+        replicates.main(self.buildout, buildout_directory, workspace)
 
     def update(self):
         return self.install()
